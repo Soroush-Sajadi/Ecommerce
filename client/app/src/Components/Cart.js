39360ef7f7a073b2351915b68966f9816cd26d34
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-05-23 21:08:18 +0200

    css

--- client/app/src/Components/Cart.js
+++ client/app/src/Components/Cart.js
@@ -70,20 +70,79 @@ export default class Produkter extends Component {
     }
     render() {
       return ( 
-        <div className="wraper">
-        {this.state.localData?.map(item => 
-          <div className="cart">
-            <img src={item.image} />
-          <ul className="cart-wraper">
-            <li className="li-title">{item.name}</li>
-            <li className="li-info">{item.color}</li>
-            <li className="li-desc">{item.description}</li>
+      <main>
+        
+        
+      <div className="basket">
+       
+        <div className="basket-labels">
+          <ul>
+            <li className="item item-heading">Item</li>
+            <li className="price">Price</li>
+            <li className="quantity">Quantity</li>
+            <li className="subtotal">Subtotal</li>
           </ul>
-            <input type="submit" value="Ta bort" id={item.id} name={item.name} onClick={this.removeFromCart} />
+        </div>
+        {this.state.localData.map(item => <div>
+        <div className="basket-product">
+          <div className="item">
+            <div className="product-image">
+              <img src="http://placehold.it/120x166" alt="Placholder Image 2" className="product-frame"/>
+            </div>
+            <div className="product-details">
+              <h1><strong><span className="item-quantity"></span>{item.name}</strong></h1>
+              <p><strong>Navy, Size 18</strong></p>
+              <p>Product Code - 232321939</p>
+            </div>
           </div>
-          )} 
-          {this.props.cartInfo.length !== 0 ? (<NavLink to={"/produkter/"+ this.props.cartInfo[ (this.props.cartInfo.length) - 1 ].name }> tillbacks</NavLink>):null}
-        </div>  
+          <div className="price">26.00</div>
+          <div className="quantity">
+            <input type="number" value="1" min="1" className="quantity-field"/>
+          </div>
+          <div className="subtotal">104.00</div>
+          <div className="remove">
+            <button  id={item.id} name={item.name} onClick={this.removeFromCart}>Remove</button>
+          </div>
+        </div>
+        </div>
+      )}
+        
+      </div>
+      <aside>
+        <div className="summary">
+          <div className="summary-total-items"><span className="total-items"></span> Items in your Bag</div>
+          <div className="summary-subtotal">
+            <div className="subtotal-title">Subtotal</div>
+            <div className="subtotal-value final-value" id="basket-subtotal">130.00</div>
+            <div className="summary-promo hide">
+              <div className="promo-title">Promotion</div>
+              <div className="promo-value final-value" id="basket-promo"></div>
+            </div>
+          </div>
+          <div className="summary-delivery">
+            <select name="delivery-collection" className="summary-delivery-selection">
+                <option value="0" selected="selected">Select Collection or Delivery</option>
+              <option value="collection">Collection</option>
+              <option value="first-className">Royal Mail 1st className</option>
+              <option value="second-className">Royal Mail 2nd className</option>
+              <option value="signed-for">Royal Mail Special Delivery</option>
+            </select>
+          </div>
+          <div className="summary-total">
+            <div className="total-title">Total</div>
+            <div className="total-value final-value" id="basket-total">130.00</div>
+          </div>
+          <div className="summary-checkout">
+            <button className="checkout-cta">Go to Secure Checkout</button>
+          </div>
+        </div>
+      </aside>
+      
+  </main>
+        
       )
   }
 }
+
+
+  
