4b78d8369f706e575fb46a556f2dcc16b08680d7
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-05-26 21:15:06 +0200

    checkout

--- client/app/src/Components/Checkout.js
+++ client/app/src/Components/Checkout.js
@@ -1,13 +1,25 @@
 import React, {Component} from "react";
+import './Checkout.css'
 
 export default class Omoss extends Component {
     
       
       render() {
         return (  
-            <div>
-              <h1>Let's checkout!</h1>
-            </div>
+        <>
+        <h1>Elegant Contact Form</h1>
+        <form className="cf">
+        <div className="half left cf">
+            <input type="text" id="input-name" placeholder="Name"/>
+            <input type="email" id="input-email" placeholder="Email address" />
+            <input type="text" id="input-subject" placeholder="Subject" />
+        </div>
+        <div className="half right cf">
+            <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+        </div>  
+        <input type="submit" value="Submit" id="input-submit"/>
+        </form>
+        </>
         )
     }
 }
