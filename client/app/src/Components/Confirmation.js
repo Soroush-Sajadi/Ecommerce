210e7e077a1a35ca33dac6da9908aa15cc50a760
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-06-03 15:25:25 +0200

    End of the day

--- client/app/src/Components/Confirmation.js
+++ client/app/src/Components/Confirmation.js
@@ -1,13 +1,20 @@
 import React, {Component} from "react";
 import './Confirmation.css'
 export default class Confirmation extends Component {
-    
-      
-      render() {
-        return (  
-            <div >
-              <h1 className="confirm">Way to go its done!</h1>
-            </div>
-        )
+
+    clearLocalSrorage = () => {
+      window.localStorage.clear();
     }
+    
+    componentDidMount = () => {
+      this.clearLocalSrorage();
+    }
+      
+    render() {
+      return (  
+          <div >
+            <h1 className="confirm">Way to go its done!</h1>
+          </div>
+      )
+  }
 }
